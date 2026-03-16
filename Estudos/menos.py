b896016ededc3e4370eeb2bd6b86e5245ee3563f
victor4500 <sla133278@gmail.com>
2026-03-16 14:35:47 -0300

    conta de adição

---
+++ Estudos/menos.py
@@ -0,0 +1,7 @@
+valor1 = int ( input ("digite o valor que vc quer dimuir: "))
+
+valor2 = int  (input("o outro valor: "))
+
+subtração = (valor1 - valor2 )
+
+print (f'valor que vc teve no final é: {subtração}')
