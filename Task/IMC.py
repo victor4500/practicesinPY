7c88e48fa279e989041b51d62d223b8f1018997d
victor4500 <sla133278@gmail.com>
2026-04-01 23:06:29 -0300

    criando um simples IMC

---
+++ Task/IMC.py
@@ -0,0 +1,21 @@
+print ("---sua calculado de IMC---")
+
+print()
+
+altura = float (input("   Digite sua altura: "))
+
+peso = float (input("   digite seu peso: "))
+
+altura2 = (altura * altura) 
+
+imcfinal = (peso / altura2)
+
+print (f"seu IME é {imcfinal:.2f}")
+
+if imcfinal <= 24.9:
+    print("vc esta na medida do seu peso, fique tranquilo.")
+elif imcfinal <= 29.9:
+    print("vc ta com sobrepeso, seria bom dar uma olhada em receirtas ou até mesmo fazer enxercicios.")
+elif imcfinal > 30:
+    print("ta gordão, tem que fazer alguma coisa agora antes que seja tarde dmias")
+    
