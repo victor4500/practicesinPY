18e242ac769be5d2d0b237a1dbe72ceb7eb25afe
victor4500 <sla133278@gmail.com>
2026-04-18 16:11:46 -0300

    add de numero

--- logicabasica/adivinhar.py
+++ logicabasica/adivinhar.py
@@ -3,7 +3,7 @@ print("aqui e pra vc tentar acertar o numero")
 print("*************************************")
 
 numero = 10
-
+numero = 1
 chute = int (input("digita o numero que vc acredita que seja: "))
 
 print(f"voce digitou: {chute}")
