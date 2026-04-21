85098a4efaf87829d8dd204bdf4201b3baf18ab5
victor4500 <sla133278@gmail.com>
2026-04-20 22:08:07 -0300

    adicionando numeros de tentativas e utilizando laço while

--- logicabasica/adivinhar.py
+++ logicabasica/adivinhar.py
@@ -3,18 +3,25 @@ print("aqui e pra vc tentar acertar o numero")
 print("*************************************")
 
 numero = 10
-numero = 1
-chute = int (input("digita o numero que vc acredita que seja: "))
 
-print(f"voce digitou: {chute}")
+tdtentativas = 3
 
-taCerto = chute == numero
-taMaior = chute > numero
+rodada = 1
 
-if (taCerto):
-    print("vc acertou!!!")
-else:
-    if(taMaior):
-        print("vc errou! esta numeros a cima.")
+while (rodada <= tdtentativas):
+    chute = int (input("digita o numero que vc acredita que seja: "))
+    print(f"tá na rodada {rodada} de {tdtentativas} tentativas.")
+    print(f"voce digitou: {chute}")
+
+    taCerto = chute == numero
+    taMaior = chute > numero
+
+    if (taCerto):
+        print("vc acertou!!!")
     else:
-        print("ta errdo! ta com numeros a penos ai.")
+        if(taMaior):
+            print("vc errou! esta numeros a cima.")
+        else:
+            print("ta errdo! ta com numeros a penos ai.")
+
+    rodada = rodada + 1
