b76197670dbb98de192f8fd3ec22ac89d52c0542
victor4500 <sla133278@gmail.com>
2026-04-21 11:57:17 -0300

    adicionando if e continue nos loops

--- logicabasica/adivinhar.py
+++ logicabasica/adivinhar.py
@@ -8,16 +8,22 @@ tdtentativas = 3
 
 rodada = 1
 
-while (rodada <= tdtentativas):
+while (rodada <= tdtentativas ):
     chute = int (input("digita o numero que vc acredita que seja: "))
     print(f"tá na rodada {rodada} de {tdtentativas} tentativas.")
     print(f"voce digitou: {chute}")
+
+    if(chute < 1 or chute > 10):
+        print("o numero deve ser entre 1 e 10 ")
+        rodada = rodada + 1
+        continue
 
     taCerto = chute == numero
     taMaior = chute > numero
 
     if (taCerto):
         print("vc acertou!!!")
+        break
     else:
         if(taMaior):
             print("vc errou! esta numeros a cima.")
--- logicabasica/adivinharFor.py
+++ logicabasica/adivinharFor.py
@@ -11,12 +11,16 @@ for rodada in range(1, tdtentativas + 1):
      print(f"ta na rodada {rodada} de {tdtentativas} tentativas")
      print(f"vc digitou: {chute}")
 
+     if(chute < 1 or chute > 20):
+          print("o numero esta entre 1 e 20!")
+          continue
      taCerto = chute == numero
      taMaior = chute > numero
      taMenor = chute < numero
 
      if(taCerto):
           print(f"voce acertou, o numero é {chute}")
+          break
      else:
           if(taMaior):
                print("vc errou, tem numeros a mais")
